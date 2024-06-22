f8ea64c79b06c4359050b9bdc92698a0020d1eb9
chyijia <chyijia@google.com>
2024-06-22 02:13:31 +0000

    Support fetching the last known good build
    When users pass `-branch` and not `-build_id`, it'll fetch the latest
    passed build ID in the given branch and the given target.
    
    Bug: 349534024
    Test: fetch_artifact -target=aosp_arm64-trunk_staging-userdebug
    -branch=aosp-main -artifact=COPIED
    
    Change-Id: Ifee522be4a7829b0044948913890d77a0904bc0d

--- fetch_artifact.go
+++ fetch_artifact.go
@@ -15,24 +15,55 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"os"
 	"path"
 )
 
-var target = flag.String("target", "", "the target to fetch from")
-var buildID = flag.String("build_id", "", "the build id to fetch from")
-var artifact = flag.String("artifact", "", "the artifact to download")
-var output = flag.String("output", "", "the file name to save as")
+var (
+	target   = flag.String("target", "", "the target to fetch from")
+	buildID  = flag.String("build_id", "", "the build id to fetch from, can use '-branch' to get the latest passed build ID")
+	branch   = flag.String("branch", "", "the branch to fetch from, used when '-build_id' is not provided,\nit would fetch the latest successful build")
+	artifact = flag.String("artifact", "", "the artifact to download")
+	output   = flag.String("output", "", "the file name to save as")
+)
+
 var writeToStdout = false
+
+type BuildResponse struct {
+	Builds []Build `json:"builds"`
+}
+
+type Build struct {
+	BuildId string `json:"buildId"`
+}
 
 func errPrint(msg string) {
 	fmt.Fprintln(os.Stderr, msg)
 	os.Exit(1)
+}
+
+func checkFlags() error {
+	if len(*target) == 0 {
+		return errors.New("missing target")
+	}
+	if len(*artifact) == 0 {
+		return errors.New("missing artifact")
+	}
+	if len(*buildID) == 0 && len(*branch) == 0 {
+		return errors.New("missing build_id or branch")
+	}
+	if len(*buildID) != 0 && len(*branch) != 0 {
+		return errors.New("too many arguments. give only build ID or branch")
+	}
+	return nil
 }
 
 func main() {
@@ -56,18 +87,45 @@ func main() {
 		}
 	}
 
-	url := fmt.Sprintf("https://androidbuildinternal.googleapis.com/android/internal/build/v3/builds/%s/%s/attempts/latest/artifacts/%s/url", *buildID, *target, *artifact)
+	if err := checkFlags(); err != nil {
+		flag.Usage()
+		errPrint(err.Error())
+	}
 
+	client := &http.Client{}
+
+	if len(*buildID) == 0 {
+		latestGoodBuildID, err := getLatestGoodBuild(client, *branch, *target)
+		if err != nil {
+			errPrint(fmt.Sprintf("Error fetching latest good buildID %s, err", err))
+		}
+		flag.Set("build_id", latestGoodBuildID)
+	}
+	err := fetchArtifact(client, *target, *buildID, *artifact, *output)
+	if err != nil {
+		errPrint(fmt.Sprintf("Fetch artifact error: %s", err))
+	}
+}
+
+func sendRequest(client *http.Client, url string) (*http.Response, error) {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		errPrint(fmt.Sprintf("unable to build request %v", err))
+		return nil, err
 	}
 	req.Header.Set("Accept", "application/json")
 
-	client := http.Client{}
 	res, err := client.Do(req)
 	if err != nil {
-		errPrint(fmt.Sprintf("Unable to make request %s.", err))
+		return nil, err
+	}
+	return res, nil
+}
+
+func fetchArtifact(client *http.Client, target string, buildID string, artifact string, output string) error {
+	url := fmt.Sprintf("https://androidbuildinternal.googleapis.com/android/internal/build/v3/builds/%s/%s/attempts/latest/artifacts/%s/url", url.QueryEscape(buildID), url.QueryEscape(target), url.QueryEscape(artifact))
+	res, err := sendRequest(client, url)
+	if err != nil {
+		return fmt.Errorf("error fetching artifact %w", err)
 	}
 	defer res.Body.Close()
 
@@ -78,19 +136,47 @@ func main() {
 
 	if writeToStdout {
 		io.Copy(os.Stdout, res.Body)
-		return
+		return nil
 	}
 
-	fileName := *artifact
-	if len(*output) > 0 {
-		fileName = *output
+	fileName := artifact
+	if len(output) > 0 {
+		fileName = output
 	}
 
 	f, err := os.Create(path.Base(fileName))
 	if err != nil {
-		errPrint(fmt.Sprintf("Unable to create file %s.", err))
+		return fmt.Errorf("unable to create file %w", err)
 	}
 	defer f.Close()
 	io.Copy(f, res.Body)
 	fmt.Printf("File %s created.\n", f.Name())
+	return nil
+}
+
+func getLatestGoodBuild(client *http.Client, branch string, target string) (string, error) {
+	url := fmt.Sprintf("https://androidbuildinternal.googleapis.com/android/internal/build/v3/builds?branches=%s&buildAttemptStatus=complete&buildType=submitted&maxResults=1&successful=true&target=%s", url.QueryEscape(branch), url.QueryEscape(target))
+	res, err := sendRequest(client, url)
+	if err != nil {
+		return "", fmt.Errorf("send request error: %w", err)
+	}
+	defer res.Body.Close()
+
+	if res.Status != "200 OK" {
+		body, _ := ioutil.ReadAll(res.Body)
+		return "", fmt.Errorf("unable to get Build ID: %s\n %s", res.Status, body)
+	}
+
+	body, _ := io.ReadAll(res.Body)
+	var buildData BuildResponse
+	err = json.Unmarshal(body, &buildData)
+
+	if err != nil {
+		return "", fmt.Errorf("error parsing JSON: %w", err)
+	}
+	if len(buildData.Builds) == 0 {
+		return "", errors.New("error no build ID is found")
+	}
+
+	return buildData.Builds[0].BuildId, nil
 }
